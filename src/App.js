7b6c3e3e73e89454fe2747962a3982b142836041
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-12 20:07:25 +0600

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
+import Courses from './Components/Courses/Courses';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+import MyClasses from './Components/MyClasses/MyClasses';
+import Navbar from './Components/Navbar/Navbar';
+import SignUp from './Components/SignUp/SignUp';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello</h1>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/courses' element={<Courses/>} />
+        <Route path='/myclasses' element={<MyClasses/>} />
+        <Route path='/signup' element={<SignUp/>} />
+        <Route path='/about' element={<About/>} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="bg-slate-300">
+      <div className="container mx-auto flex items-center justify-between h-20">
+        <div className="logo">
+          <h1 className="text-2xl font-bold text-blue-600">Logo</h1>
+        </div>
+        <nav>
+          <NavLink
+            to={"/"}
+            className={({ isActive }) =>
+              isActive ? "text-blue-600" : "text-slate-700"
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to={"/courses"}
+            className={({ isActive }) =>
+              isActive ? "text-blue-600" : "text-slate-700"
+            }
+          >
+            Courses
+          </NavLink>
+          <NavLink
+            to={"/myclasses"}
+            className={({ isActive }) =>
+              isActive ? "text-blue-600" : "text-slate-700"
+            }
+          >
+            My Classes
+          </NavLink>
+          <NavLink
+            to={"/signup"}
+            className={({ isActive }) =>
+              isActive ? "text-blue-600" : "text-slate-700"
+            }
+          >
+            Sing Up
+          </NavLink>
+          <NavLink
+            to={"/about"}
+            className={({ isActive }) =>
+              isActive ? "text-blue-600" : "text-slate-700"
+            }
+          >
+            About
+          </NavLink>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
